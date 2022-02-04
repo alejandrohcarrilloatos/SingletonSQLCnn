0e64b4ac02d2d0b4f8c7d9429bc1f4be2b1dab2b
Alejandro <el_grande_ahc@hotmail.com>
2022-02-04 13:12:07 -0600

    Conexion a SQL server implemetada con exito

--- SingletonSQLCnnConsoleApp/SingletonSQLCnn/Program.cs
+++ SingletonSQLCnnConsoleApp/SingletonSQLCnn/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,17 @@ namespace SingletonSQLCnn
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo");
+
+            SqlConnection conexion = SingletonDatabase.getConnection;
+
+            Console.WriteLine(conexion.ConnectionString);
+            //Console.ReadLine();
+
+            conexion = SingletonDatabase.getConnection;
+            Console.WriteLine(conexion.ConnectionString);
+
             Console.ReadLine();
+
         }
     }
 }
---
+++ SingletonSQLCnnConsoleApp/SingletonSQLCnn/SingletonDatabase.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.SqlClient;
+
+namespace SingletonSQLCnn
+{
+    public class SingletonDatabase
+    {
+        // Paso 1: Hacer privado el contructor
+        private SingletonDatabase() {
+        
+        }
+
+        // Paso 2: Crear propiedad privada para contener la instancia
+        private static SqlConnection _instance;
+        // Para manejar hilos multiples usamos un objeto para sicronizar los hilos durante el primer acceso al singleton
+        private static readonly object _syncObject = new object();
+        
+        const string strCnn = @"Server=.,14033;Database=SchoolAdmin;User Id=sa;Password=Admin4dm1n!;";
+ 
+        // Paso 3: Regresa la instancia, si no existe crea una nueva.
+        public static SqlConnection getConnection {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncObject) // Sincronizamos los hilos
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SqlConnection(strCnn);
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public static void Open()
+        {
+            if (_instance != null)
+                _instance.Open();
+        }
+        public static void Close()
+        {
+            if (_instance != null)
+                _instance.Close();
+        }
+    }
+
+}
